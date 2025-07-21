3085cc7abd37bc7d1098baccb4d5d83bc80441a9
anton-1488 <ap67253100@gmail.com>
2025-07-21 12:58:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById('service1');
+const ref = document.querySelector('serv-name');
+const txt = btn.textContent;
+
+btn.addEventListener('click', function () {
+    alert('Hello');
+});
